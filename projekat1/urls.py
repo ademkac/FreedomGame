529b1010e09499b2abce19923e21bd15e71521fe
ademkac <32670928+ademkac@users.noreply.github.com>
2021-09-13 19:16:41 +0200

    Update urls.py

--- projekat1/urls.py
+++ projekat1/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^sendstate$', views.sendstate, name='sendstate'),
     re_path(r'^sendstate1$', views.sendstate1, name='sendstate1'),
+    re_path(r'^sendstate2$', views.sendstate2, name='sendstate2'),
+
 ]
